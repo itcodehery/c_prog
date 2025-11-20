ea83f89e7a1b61f198515591c5639076020823db
Hari Prasad <haririo321@gmail.com>
2025-11-20 11:43:14 +0530

    added dijkstra's algorithm

---
+++ fibonacci_opti/fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// int fib(int index, int n) {
+//   if (n <= index) {
+//     return n;
+//   } else {
+//     printf("\n%d", index);
+//     return (index + fib(index + 1, n));
+//   }
+// }
+int fib(int n, int *memo) {
+  if (n <= 1) {
+    return n;
+  } else {
+  }
+}
+
+int contains(int memo[], int value) {
+  for (int i = 0; i < 5; i++) {
+  }
+}
+
+int main() { printf("%d", fib(0, 5)); }
